2e9ea55f68eebe159f5bab1eeea7fea2f9024940
kuabrs <95240062+kuabrs@users.noreply.github.com>
2022-10-17 15:56:24 -0300

    adicionei aula 1

---
+++ Aula01/AgendaApp/Contato.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaApp
+{
+    class Contato
+    {
+        private string nome, email, tel;
+        public Contato(string n, string e, string f)
+        {
+            nome = n;
+            email = e;
+            tel = f;
+        }
+        public override string ToString()
+        {
+            return $"{nome}, {email}, {tel}";
+        }
+
+    }
+}
---
+++ Aula01/AgendaApp/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AgendaApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show(TxtNome.Text);
+            Contato c = new Contato(TxtNome.Text, TxtEmail.Text, TxtTel.Text);
+            List.Items.Add(c);
+      }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (List.SelectedItem != null)
+                List.Items.Remove(List.SelectedItem);
+        }
+    }
+}
