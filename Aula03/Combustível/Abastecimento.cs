078d1b75e899dfeca8314afbf957851fabd8e582
kuabrs <kaua.b@escolar.ifrn.edu.br>
2022-10-24 15:49:56 -0300

    atualizei Aula03

---
+++ Aula03/Combustível/Abastecimento.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combustível
+{
+    class Abastecimento
+    {
+        private double Vl, Vp, kmr;
+        public void SetValorL(double v)
+        {
+            if (v > 0) Vl = v;
+        }
+        public void SetValorP(double v)
+        {
+            if (v > 0) Vp = v;
+        }
+        public void Setkmr(double v)
+        {
+            if (v > 0) kmr = v;
+        }
+        public double Mkml()
+        {
+            return kmr / (Vp / Vl);
+        }
+        public double MVkm() => Vp / kmr;
+        
+    }
+}
---
+++ Aula03/Combustível/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Combustível
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Abastecimento x = new Abastecimento();
+            x.Setkmr(double.Parse(kmr.Text));
+            x.SetValorL(double.Parse(vll.Text)); 
+            x.SetValorP(double.Parse(vlp.Text));
+            rkm.Text = x.MVkm().ToString("0.00");
+            kml.Text = x.Mkml().ToString("0.00");
+        }
+    }
+}
